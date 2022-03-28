5d74aa05a727d90596b8e31505d2c488f645261b
Thomascystosis <thom.clarke@gmail.com>
2022-03-28 14:13:41 -0600

    setup for heroku, port listen, gitignore

--- app.js
+++ app.js
@@ -153,6 +153,14 @@ app.get("/about", function(req, res){
   res.render("about");
 });
 
-app.listen(3000, function() {
+//heroku listen port
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+// app.listen(port);
+
+//local server
+app.listen(port, function() {
   console.log("Server started on port 3000");
 });
