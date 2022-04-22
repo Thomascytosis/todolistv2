8d17b1f73899c7ddb43ddfcd8d7650810617ddba
Thomascystosis <thom.clarke@gmail.com>
2022-04-22 13:31:11 -0600

    env setup 4 to heroku

--- app.js
+++ app.js
@@ -15,15 +15,10 @@ app.use(express.static("public"));
 // Arrays used to collect user input
 // const items = ["Buy Food", "Cook Food", "Eat Food"];
 // const workItems = [];
-const aws = require('aws-sdk');
 
-let s3 = new aws.S3({
-  accessKeyId: process.env.MONGO_ID,
-  secretAccessKey: process.env.MONGO_PSW
-});
 //Use Mongoose for collecting user input
 // mongoose.connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser: true});
-mongoose.connect("mongodb+srv://{s3.accessKeyId}:{s3.secretAccessKey}@todocluster.xqqgv.mongodb.net/todolistDB?retryWrites=true&w=majority");
+mongoose.connect("mongodb+srv://"+process.env.MONGO_ID+":"+process.env.MONGO_PSW+"@todocluster.xqqgv.mongodb.net/todolistDB?retryWrites=true&w=majority");
 
 
 const itemsSchema = {
